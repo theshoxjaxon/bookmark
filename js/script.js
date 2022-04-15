5d353b7947fbeb8bae76e22d5da7e86cc4da1540
SHOX <theshoxjaxon@gmail.com>
2022-04-15 21:38:53 +0500

    add new hamburger menyu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 let elHamburger = document.querySelector(".hamburger-menyu-btn");
+let elHeader = document.querySelector(".header");
+elHamburger.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  elHeader.classList.toggle("header--open");
+});
